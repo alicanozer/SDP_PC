73b2ed2319bc913cf0b16c28e44fc9c0a2d1aa18
Bilyan Borisov <bilyan.borisov@gmail.com>
2014-02-03 02:09:35 +0000

    adding some multithreading code but it doesn't appear to run faster..

--- SDP_PC/src/vision/TestVision.java
+++ SDP_PC/src/vision/TestVision.java
@@ -15,25 +15,41 @@ public class TestVision {
 	public static void main(String[] args) throws IOException{
 		// finding the field
 		BufferedImage img1 = ImageIO.read(new File("test_images/00000007.jpg"));
+		
+//		VisionOps v = new VisionOps(img1);
+//		try {
+//			v.getObjectLocations();
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		long begTest = new java.util.Date().getTime();
 		Point2D_I32 ball = VisionOps.findBall(img1);
 		Point2D_I32[] yellowMarkers = VisionOps.findYellowMarkers(img1);
 		Point2D_I32[] blueMarkers = VisionOps.findBlueMarkers(img1);
-		Graphics2D g = (Graphics2D) img1.getGraphics();
-		// drawing X over ball
-		g.drawLine(ball.x - 10, ball.y , ball.x + 10, ball.y );
-		g.drawLine(ball.x , ball.y - 10, ball.x , ball.y + 10);
-		// drawing X over yellow markers
-		g.drawLine(yellowMarkers[0].x - 10, yellowMarkers[0].y, yellowMarkers[0].x + 10, yellowMarkers[0].y);
-		g.drawLine(yellowMarkers[0].x , yellowMarkers[0].y + 10, yellowMarkers[0].x , yellowMarkers[0].y - 10);
-		
-		g.drawLine(yellowMarkers[1].x - 10, yellowMarkers[1].y, yellowMarkers[1].x + 10, yellowMarkers[1].y);
-		g.drawLine(yellowMarkers[1].x , yellowMarkers[1].y + 10, yellowMarkers[1].x , yellowMarkers[1].y - 10);
-		// drawing X over blue markers
-		g.drawLine(blueMarkers[0].x - 10, blueMarkers[0].y, blueMarkers[0].x + 10, blueMarkers[0].y);
-		g.drawLine(blueMarkers[0].x , blueMarkers[0].y + 10, blueMarkers[0].x , blueMarkers[0].y - 10);
-		
-		g.drawLine(blueMarkers[1].x - 10, blueMarkers[1].y, blueMarkers[1].x + 10, blueMarkers[1].y);
-		g.drawLine(blueMarkers[1].x , blueMarkers[1].y + 10, blueMarkers[1].x , blueMarkers[1].y - 10);
-		ShowImages.showWindow(img1,"identifying objects");
+		Point2D_I32[] dots = VisionOps.findDots(img1);
+		Double secs = new Double((new java.util.Date().getTime() - begTest)*0.001);
+		System.out.println("run time no threads" + secs + " secs");
+//
+//		
+//		
+//		Graphics2D g = (Graphics2D) img1.getGraphics();
+//		// drawing X over ball
+//		g.drawLine(ball.x - 10, ball.y , ball.x + 10, ball.y );
+//		g.drawLine(ball.x , ball.y - 10, ball.x , ball.y + 10);
+//		// drawing X over yellow markers
+//		g.drawLine(yellowMarkers[0].x - 10, yellowMarkers[0].y, yellowMarkers[0].x + 10, yellowMarkers[0].y);
+//		g.drawLine(yellowMarkers[0].x , yellowMarkers[0].y + 10, yellowMarkers[0].x , yellowMarkers[0].y - 10);
+//		
+//		g.drawLine(yellowMarkers[1].x - 10, yellowMarkers[1].y, yellowMarkers[1].x + 10, yellowMarkers[1].y);
+//		g.drawLine(yellowMarkers[1].x , yellowMarkers[1].y + 10, yellowMarkers[1].x , yellowMarkers[1].y - 10);
+//		// drawing X over blue markers
+//		g.drawLine(blueMarkers[0].x - 10, blueMarkers[0].y, blueMarkers[0].x + 10, blueMarkers[0].y);
+//		g.drawLine(blueMarkers[0].x , blueMarkers[0].y + 10, blueMarkers[0].x , blueMarkers[0].y - 10);
+//		
+//		g.drawLine(blueMarkers[1].x - 10, blueMarkers[1].y, blueMarkers[1].x + 10, blueMarkers[1].y);
+//		g.drawLine(blueMarkers[1].x , blueMarkers[1].y + 10, blueMarkers[1].x , blueMarkers[1].y - 10);
+//		ShowImages.showWindow(img1,"identifying objects");
 	}
 }
