24fe9d23b5aadf7f7c338a4de870b4b1d998065c
Bilyan Borisov <bilyan.borisov@gmail.com>
2014-02-03 02:07:19 +0000

    Adding a simple wrapper class that contains the locations of all objects of interest

---
+++ SDP_PC/src/vision/ObjectLocations.java
@@ -0,0 +1,25 @@
+package vision;
+
+import georegression.struct.point.Point2D_I32;
+/**
+ * stub wrapper around object positions
+ * @author bilyan
+ *
+ */
+public class ObjectLocations {
+	public Point2D_I32 ball;
+	public Point2D_I32[] yellowMarkers;
+	public Point2D_I32[] blueMarkers;
+	public Point2D_I32[] dots;
+
+	public ObjectLocations(
+			Point2D_I32 ball,
+			Point2D_I32[] yellowMarkers,
+			Point2D_I32[] blueMarkers,
+			Point2D_I32[] dots){
+		this.ball = ball;
+		this.yellowMarkers = yellowMarkers;
+		this.blueMarkers = blueMarkers;
+		this.dots = dots;
+	}
+}
