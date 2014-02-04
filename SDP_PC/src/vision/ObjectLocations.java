5347fe1ae036475879ca28478afb1c384dc2f007
Bilyan Borisov <bilyan.borisov@gmail.com>
2014-02-04 18:17:04 +0000

    fixed draw crosses - now it doesnt cause null pointer dereferences

--- SDP_PC/src/vision/ObjectLocations.java
+++ SDP_PC/src/vision/ObjectLocations.java
@@ -1,5 +1,6 @@
 package vision;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
 
 import georegression.struct.point.Point2D_I32;
@@ -29,40 +30,60 @@ public class ObjectLocations {
 	 * @param g
 	 */
 	public void drawCrosses(Graphics2D g){
+		Color c = g.getColor();
+		g.setColor(Color.WHITE);
 		if (this.ball != null) {
-			// drawing X over ball
+			// drawing X over ball			//		
 			g.drawLine(this.ball.x - 10, this.ball.y, this.ball.x + 10, this.ball.y);
 			g.drawLine(this.ball.x, this.ball.y - 10, this.ball.x, this.ball.y + 10);
 		}
 		if (this.yellowMarkers != null) {
-			g.drawLine(this.yellowMarkers[0].x - 10, this.yellowMarkers[0].y,
-					this.yellowMarkers[0].x + 10, this.yellowMarkers[0].y);
-			g.drawLine(this.yellowMarkers[0].x, this.yellowMarkers[0].y + 10,
-					this.yellowMarkers[0].x, this.yellowMarkers[0].y - 10);
+			g.drawLine(this.yellowMarkers[0].x - 10, 
+					this.yellowMarkers[0].y,
+					this.yellowMarkers[0].x + 10, 
+					this.yellowMarkers[0].y);
+			g.drawLine(this.yellowMarkers[0].x, 
+					this.yellowMarkers[0].y + 10,
+					this.yellowMarkers[0].x, 
+					this.yellowMarkers[0].y - 10);
 			
-			if (this.yellowMarkers.length == 2) {
+
+			try {
 				g.drawLine(this.yellowMarkers[1].x - 10,
-						this.yellowMarkers[1].y, this.yellowMarkers[1].x + 10,
+						this.yellowMarkers[1].y, 
+						this.yellowMarkers[1].x + 10,
 						this.yellowMarkers[1].y);
 				g.drawLine(this.yellowMarkers[1].x,
-						this.yellowMarkers[1].y + 10, this.yellowMarkers[1].x,
+						this.yellowMarkers[1].y + 10, 
+						this.yellowMarkers[1].x,
 						this.yellowMarkers[1].y - 10);
+			} catch (NullPointerException e) {
 			}
 		}
 		if (this.blueMarkers != null) {
-			//		// drawing X over blue markers
-			g.drawLine(this.blueMarkers[0].x - 10, this.blueMarkers[0].y,
-					this.blueMarkers[0].x + 10, this.blueMarkers[0].y);
-			g.drawLine(this.blueMarkers[0].x, this.blueMarkers[0].y + 10,
-					this.blueMarkers[0].x, this.blueMarkers[0].y - 10);
-			
-			if (this.blueMarkers.length == 2) {
-				g.drawLine(this.blueMarkers[1].x - 10, this.blueMarkers[1].y,
-						this.blueMarkers[1].x + 10, this.blueMarkers[1].y);
-				g.drawLine(this.blueMarkers[1].x, this.blueMarkers[1].y + 10,
-						this.blueMarkers[1].x, this.blueMarkers[1].y - 10);
+			// drawing X over blue markers
+			g.drawLine(this.blueMarkers[0].x - 10, 
+					this.blueMarkers[0].y,
+					this.blueMarkers[0].x + 10, 
+					this.blueMarkers[0].y);
+			g.drawLine(this.blueMarkers[0].x, 
+					this.blueMarkers[0].y + 10,
+					this.blueMarkers[0].x, 
+					this.blueMarkers[0].y - 10);
+
+			try {
+				g.drawLine(this.blueMarkers[1].x - 10, 
+						this.blueMarkers[1].y,
+						this.blueMarkers[1].x + 10, 
+						this.blueMarkers[1].y);
+				g.drawLine(this.blueMarkers[1].x, 
+						this.blueMarkers[1].y + 10,
+						this.blueMarkers[1].x, 
+						this.blueMarkers[1].y - 10);
+			} catch (NullPointerException e) {
 			}
 		}
+		g.setColor(c);
 	}
 	
 }
