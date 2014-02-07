9a10608e4d21f93aef7d3c0f2879c1b32c9d4436
Bilyan Borisov <bilyan.borisov@gmail.com>
2014-02-07 15:09:55 +0000

    Changing dot location function to ignore large/small contours

--- SDP_PC/src/vision/TestVision.java
+++ SDP_PC/src/vision/TestVision.java
@@ -32,6 +32,8 @@ public class TestVision {
 		BufferedImage img1 = ImageIO.read(new File("test_images/00000008.jpg"));
 
 		BufferedImage img2 = ImageIO.read(new File("test_images/000000011.jpg"));
+		
+		BufferedImage img3 = ImageIO.read(new File("test_images/cropped5.jpg"));
 
 		ObjectLocations.setRegions(img2);
 		ObjectLocations.setYellowDefendingLeft(true);
@@ -43,9 +45,34 @@ public class TestVision {
 		float[] hues = {0.5f};
 		float[] saturations = {0.4f};
 		//img2 = ConvertBufferedImage.convertTo_F32(VisionOps.segmentMultiHSV(img2, hues, saturations)[0], null, true);
-		img2 = VisionOps.contourOps("lines", VisionOps.segmentMultiHSV(img2, hues, saturations)[0]);
+		//img2 = VisionOps.contourOps("lines", VisionOps.segmentMultiHSV(img2, hues, saturations)[0]);
 		
-		ShowImages.showWindow(img2,"identifying objects");
+		MultiSpectral<ImageFloat32> input = ConvertBufferedImage.convertFromMulti(img3,null,true,ImageFloat32.class);
+		
+		MultiSpectral<ImageFloat32> hsv = new MultiSpectral<ImageFloat32>(ImageFloat32.class,img3.getWidth(),img3.getHeight(),3);
+		ImageUInt8 binary = new ImageUInt8(input.width,input.height);
+
+		// Convert into HSV
+		ColorHsv.rgbToHsv_F32(input,hsv);
+
+		ThresholdImageOps.threshold(hsv.getBand(2),binary,(float)65,true);
+		
+		ImageUInt8 filtered = BinaryImageOps.erode8(binary,null);
+		filtered = BinaryImageOps.dilate8(filtered, null);
+		List<Contour> contours = BinaryImageOps.contour(filtered, 8, null);
+		
+		
+		System.out.println(VisionOps.getMeanDotNearMarker(img3, new Point2D_I32(19,19), 30));
+		
+		img3 = VisualizeBinaryData.renderContours(
+				contours,
+				0xFFFFFF,
+				0xFF20FF,
+				input.getWidth(),
+				input.getHeight(),
+				null);
+		
+		ShowImages.showWindow(img3,"identifying objects");
 		
 		
 		
