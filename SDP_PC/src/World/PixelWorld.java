392040706896aaff800bd4addf14661217a00d5f
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-02-12 22:27:25 +0000

    The PixelWorld and RealWorld now identifies objects each in the way I expect would be more convenient - by color & position or by team affiliation. It would the be up to the world model to keep track of which color and which side of the pitch each team is on.

--- SDP_PC/src/World/PixelWorld.java
+++ SDP_PC/src/World/PixelWorld.java
@@ -4,7 +4,25 @@ package world;
  * An interface defining what methods will be available to get a representation of the world. Pixel 
  * implies that everything is exactly as it appears in the image and the primary unit is pixels.
  */
-public interface PixelWorld extends World{
+public interface PixelWorld{
+	//Integers constants to be used to reference the various moving elements in the world.
+	//(I.e. the four robots and the ball)
+	public static final int YELLOW_ATTACKER = 1;
+	public static final int YELLOW_DEFENDER = 2;
+	public static final int BLUE_ATTACKER = 3;
+	public static final int BLUE_DEFEMDER = 4;
+	public static final int BALL = 5;
+	
+	//Integers constants to be used to reference the various stationary elements in the world.
+	//(I.e. the four zones and the two goals)
+	public static final int LEFT_DEFENDER_ZONE = 6;
+	public static final int RIGHT_ATTACKER_ZONE = 7;
+	public static final int LEFT_ATTACKER_ZONE = 8;
+	public static final int RIGHT_DEFENDER_ZONE = 9;
+	
+	public static final int LEFT_GOAL = 10;
+	public static final int RIGHT_GOAL = 11;
+	
 	/**
 	 * Get one of the mobile objects in the world.
 	 * 
--- SDP_PC/src/World/RealWorld.java
+++ SDP_PC/src/World/RealWorld.java
@@ -5,7 +5,25 @@ package world;
  * that units will be cm rather than pixels and that the output may have been processed to compensate for 
  * perspective projection, barrel distortion etc.
  */
-public interface RealWorld extends World{
+public interface RealWorld {
+	//Integers constants to be used to reference the various moving elements in the world.
+	//(I.e. the four robots and the ball)
+	public static final int HERO_ATTACKER = 1;
+	public static final int HERO_DEFENDER = 2;
+	public static final int VILLAIN_ATTACKER = 3;
+	public static final int VILLAIN_DEFEMDER = 4;
+	public static final int BALL = 5;
+	
+	//Integers constants to be used to reference the various stationary elements in the world.
+	//(I.e. the four zones and the two goals)
+	public static final int HERO_DEFENDER_ZONE = 6;
+	public static final int VILLAIN_ATTACKER_ZONE = 7;
+	public static final int HERO_ATTACKER_ZONE = 8;
+	public static final int VILLAIN_DEFENDER_ZONE = 9;
+	
+	public static final int HERO_GOAL = 10;
+	public static final int VILLAIN_GOAL = 11;
+	
 	/**
 	 * Get one of the mobile objects in the world.
 	 * 
