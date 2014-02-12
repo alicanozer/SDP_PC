a9789778fdd265d9cfada2927914557ddb06a701
ccimrie <imrieimrie@gmail.com>
2014-02-12 11:43:21 +0000

    asd

--- SDP_PC/src/vision/SimpleViewer.java
+++ SDP_PC/src/vision/SimpleViewer.java
@@ -194,7 +194,7 @@ public class SimpleViewer extends WindowAdapter implements CaptureCallback{
 		lastFrame = thisFrame;
 		float[] hues = {0.5f}; 
 		float[] saturations = {0.4f};
-		//img = VisionOps.newDisplay(VisionOps.newHSVSegment("yellow",img),img.getWidth(), img.getHeight());
+		img = VisionOps.newDisplay(VisionOps.newHSVSegment("yellow",img),img.getWidth(), img.getHeight());
 		
 		//img = VisionOps.contourOps("lines", VisionOps.segmentMultiHSV(img, hues, saturations)[0]);
 		
