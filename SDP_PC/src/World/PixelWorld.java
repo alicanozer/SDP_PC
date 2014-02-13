8c88ce2e15b0ebd476e73f1c337bc060c320fd8f
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-02-13 15:00:35 +0000

    Modified constants to values that are more helpful when implementing the world model.

--- SDP_PC/src/World/PixelWorld.java
+++ SDP_PC/src/World/PixelWorld.java
@@ -7,21 +7,21 @@ package world;
 public interface PixelWorld{
 	//Integers constants to be used to reference the various moving elements in the world.
 	//(I.e. the four robots and the ball)
-	public static final int YELLOW_ATTACKER = 1;
-	public static final int YELLOW_DEFENDER = 2;
-	public static final int BLUE_ATTACKER = 3;
-	public static final int BLUE_DEFEMDER = 4;
-	public static final int BALL = 5;
+	public static final int YELLOW_ATTACKER = 0;
+	public static final int YELLOW_DEFENDER = 1;
+	public static final int BLUE_ATTACKER = 2;
+	public static final int BLUE_DEFEMDER = 3;
+	public static final int BALL = 4;
 	
 	//Integers constants to be used to reference the various stationary elements in the world.
 	//(I.e. the four zones and the two goals)
-	public static final int LEFT_DEFENDER_ZONE = 6;
-	public static final int RIGHT_ATTACKER_ZONE = 7;
-	public static final int LEFT_ATTACKER_ZONE = 8;
-	public static final int RIGHT_DEFENDER_ZONE = 9;
+	public static final int LEFT_TEAM_DEFENDER_ZONE = 0;
+	public static final int RIGHT_TEAM_ATTACKER_ZONE = 1;
+	public static final int LEFT_TEAM_ATTACKER_ZONE = 2;
+	public static final int RIGHT_TEAM_DEFENDER_ZONE = 3;
 	
-	public static final int LEFT_GOAL = 10;
-	public static final int RIGHT_GOAL = 11;
+	public static final int LEFT_TEAM_GOAL = 4;
+	public static final int RIGHT_TEAM_GOAL = 5;
 	
 	/**
 	 * Get one of the mobile objects in the world.
--- SDP_PC/src/World/RealWorld.java
+++ SDP_PC/src/World/RealWorld.java
@@ -8,21 +8,21 @@ package world;
 public interface RealWorld {
 	//Integers constants to be used to reference the various moving elements in the world.
 	//(I.e. the four robots and the ball)
-	public static final int HERO_ATTACKER = 1;
-	public static final int HERO_DEFENDER = 2;
-	public static final int VILLAIN_ATTACKER = 3;
-	public static final int VILLAIN_DEFEMDER = 4;
-	public static final int BALL = 5;
+	public static final int HERO_ATTACKER = 10;
+	public static final int HERO_DEFENDER = 11;
+	public static final int VILLAIN_ATTACKER = 12;
+	public static final int VILLAIN_DEFENDER = 13;
+	public static final int BALL = 14;
 	
 	//Integers constants to be used to reference the various stationary elements in the world.
 	//(I.e. the four zones and the two goals)
-	public static final int HERO_DEFENDER_ZONE = 6;
-	public static final int VILLAIN_ATTACKER_ZONE = 7;
-	public static final int HERO_ATTACKER_ZONE = 8;
-	public static final int VILLAIN_DEFENDER_ZONE = 9;
+	public static final int HERO_DEFENDER_ZONE = 10;
+	public static final int VILLAIN_ATTACKER_ZONE = 11;
+	public static final int HERO_ATTACKER_ZONE = 12;
+	public static final int VILLAIN_DEFENDER_ZONE = 13;
 	
-	public static final int HERO_GOAL = 10;
-	public static final int VILLAIN_GOAL = 11;
+	public static final int HERO_GOAL = 14;
+	public static final int VILLAIN_GOAL = 15;
 	
 	/**
 	 * Get one of the mobile objects in the world.
