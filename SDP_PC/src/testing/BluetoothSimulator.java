ff407349029e7e2a3e36f845be219377686d7e5d
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-02-14 00:14:36 +0000

    Added a very crude class to allow let strategies run without real vision and robots - these are emulated.

---
+++ SDP_PC/src/testing/BluetoothSimulator.java
@@ -0,0 +1,192 @@
+/**
+ * 
+ */
+package testing;
+
+import geometry.Vector;
+import world.MobilePixelObject;
+import world.PixelWorld;
+import comms.RobotController;
+
+/**
+ * @author apljungquist
+ * 
+ */
+public class BluetoothSimulator implements RobotController {
+
+	private boolean connected = true;
+	private boolean ready = true;
+
+	private static final long DELAY_MILLIS = 20;
+	private static final int FPS = 25;
+	
+	private double rotateSpeed = 120;
+	private double travelSpeed = 20;
+
+	Thread thread;
+
+	PixelWorld world;
+	MobilePixelObject robot;
+
+	/**
+	 * 
+	 */
+	public BluetoothSimulator(PixelWorld world, int robot) {
+		thread = new Thread();
+		thread.interrupt();
+		this.world = world;
+		this.robot = world.getMobilePixelObject(robot);
+	}
+
+	@Override
+	public void connect() throws Exception {
+		System.out.println("connect()");
+		connected = true;
+	}
+
+	@Override
+	public boolean isConnected() {
+		System.out.println("isConnected()");
+		return connected;
+	}
+
+	@Override
+	public boolean isReady() {
+		System.out.println("isReady()");
+		return ready;
+	}
+
+	@Override
+	public void disconnect() {
+		System.out.println("disconnect()");
+		connected = false;
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("stop()");
+		while (thread.isAlive()) {
+			thread.interrupt();
+			Thread.yield();
+		}
+	}
+
+	@Override
+	public void kick() {
+		System.out.println("kick()");
+		thread.interrupt();
+	}
+
+	@Override
+	public void move(final int speedX, final int speedY) {
+		stop();
+		System.out.println("move()");
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						robot.setPixelPosition(robot.getPixelPosition().add(new Vector(speedX / FPS, speedY / FPS)));
+						Thread.sleep(1000 / FPS);
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+		thread.start();
+	}
+
+	@Override
+	public void rotateLEFT(final int angle) {
+		stop();
+		System.out.println("rotateLeft()");
+		thread = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					double theta = angle;
+					while (true && theta > 0) {
+						robot.setPixelOrientation(robot.getPixelOrientation().getRotated(-rotateSpeed/FPS*Math.PI/180));
+						theta -= rotateSpeed/FPS;
+						Thread.sleep(1000 / FPS);
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+		thread.start();
+	}
+
+	@Override
+	public void rotateRIGHT(final int angle) {
+		stop();
+		System.out.println("rotateRight()");
+	thread = new Thread(new Runnable() {
+
+		@Override
+		public void run() {
+			try {
+				double theta = angle;
+				while (true && theta > 0) {
+					robot.setPixelOrientation(robot.getPixelOrientation().getRotated(rotateSpeed/FPS*Math.PI/180));
+					theta -= rotateSpeed/FPS*Math.PI/180;
+					Thread.sleep(1000 / FPS);
+				}
+			} catch (InterruptedException e) {
+			}
+		}
+	});
+	thread.start();
+	}
+
+	@Override
+	public void forward() {
+		stop();
+		System.out.println("forward()");
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						robot.setPixelPosition(robot.getPixelPosition().add(robot.getPixelOrientation().scalarMultiplication(travelSpeed/robot.getPixelOrientation().getMagnitude()/FPS)));
+						Thread.sleep(1000 / FPS);
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+		thread.start();
+	}
+
+	@Override
+	public void backwards() {
+		stop();
+		System.out.println("backwards()");
+		thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					while (true) {
+						robot.setPixelPosition(robot.getPixelPosition().subtract(robot.getPixelOrientation().scalarMultiplication(travelSpeed/robot.getPixelOrientation().getMagnitude()/FPS)));
+						Thread.sleep(1000 / FPS);
+					}
+				} catch (InterruptedException e) {
+				}
+			}
+		});
+		thread.start();
+	}
+
+	@Override
+	public void setSpeed(int speed) {
+		System.out.println("setSpeed()");
+		travelSpeed = (double)speed;
+	}
+
+	@Override
+	public boolean isMoving() {
+		System.out.println("isMoving()");
+		return thread.isAlive();
+	}
+}
---
+++ SDP_PC/src/testing/TestBluetoothSimulator.java
@@ -0,0 +1,42 @@
+package testing;
+
+import comms.RobotController;
+import world.MobilePixelObject;
+import world.World;
+
+public class TestBluetoothSimulator {
+
+	public static void main(String[] args) {
+		try {
+			World world = new World(true, true);
+			RobotController btRobot = new BluetoothSimulator(world, World.YELLOW_DEFENDER);
+			MobilePixelObject pRobot = world.getMobilePixelObject(World.YELLOW_DEFENDER);
+			
+			System.out.println(pRobot);
+			btRobot.forward();
+			Thread.sleep(1000);
+			System.out.println(pRobot);
+			btRobot.stop();
+			Thread.sleep(500);
+			System.out.println(pRobot);
+			btRobot.rotateLEFT(90);
+			Thread.sleep(4000);
+			System.out.println(pRobot);
+			btRobot.rotateLEFT(360);
+			Thread.sleep(4000);
+			System.out.println(pRobot);
+			btRobot.forward();
+			Thread.sleep(1000);
+			System.out.println(pRobot);
+			btRobot.backwards();
+			Thread.sleep(1000);
+			System.out.println(pRobot);
+			btRobot.stop();
+			
+			
+		} catch (InterruptedException e) {
+
+		}
+	}
+
+}
