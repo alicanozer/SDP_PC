baa86782140d8066838236c7d481d8b5402f6371
Eilidh Hendry <ehendry91@gmail.com>
2014-02-14 17:40:53 +0000

    Removing useless class and updating strategy interface to work with new world

--- SDP_PC/src/strategy/planning/TestStrategy.java
+++ SDP_PC/src/strategy/planning/TestStrategy.java
@@ -1,25 +1,24 @@
 package strategy.planning;
 
-import geometry.Vector;
-import movement.RobotMover;
+import comms.RobotController;
+
 import vision.ObjectLocations;
-import World.WorldState;
-import World.Ball;
-import World.Robot;
+import World.RealWorld;
+
 
 public class TestStrategy extends StrategyInterface{
 
-	private Robot ourAttackRobot;
-	private Robot ourDefenseRobot;
-	private Ball ball;
+//	private Robot ourAttackRobot;
+//	private Robot ourDefenseRobot;
+//	private Ball ball;
 	
 	ObjectLocations objs;
 	
-	public TestStrategy(RobotMover mover) {
-		super(mover);
+	public TestStrategy(RealWorld world, RobotController robot) {
+		super(world, robot);
 		
-		ball = new Ball();
-		ball.setPosition(new Vector(ObjectLocations.getBall().x,ObjectLocations.getBall().y));
+//		ball = new Ball();
+//		ball.setPosition(new Vector(ObjectLocations.getBall().x,ObjectLocations.getBall().y));
 
 
 		
