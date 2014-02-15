906cd328b0c52e0d98aab25dc8185a1e935d8ea2
Bilyan Borisov <bilyan.borisov@gmail.com>
2014-02-15 15:36:48 +0000

    adding some boofCV code and some test video files

---
+++ SDP_PC/src/vision/KMeans.java
@@ -0,0 +1,20 @@
+package vision;
+
+import java.awt.image.BufferedImage;
+
+public class KMeans {
+
+	public static BufferedImage Cluster(BufferedImage img, int k){
+		
+		
+		return null;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
