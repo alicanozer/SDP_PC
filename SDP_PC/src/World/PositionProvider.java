c1cd0f498bed326c99b9e1124f4bc15dc0425226
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-02-18 18:05:08 +0000

    Modified PositionProvider as to allow the user to know if it has already asked for the latest position.

--- SDP_PC/src/World/PositionProvider.java
+++ SDP_PC/src/World/PositionProvider.java
@@ -22,7 +22,7 @@ public interface PositionProvider {
 	 *             not be found in the requested frame, an exception will be
 	 *             thrown.
 	 */
-	public TimedVector getRobot0Position(int frameNumber) throws Exception;
+	public TimedVector getRobot0Position(long frameNumber) throws Exception;
 
 	/**
 	 * Get the position of the 2nd (/4) robot from the left in the plating
@@ -38,7 +38,7 @@ public interface PositionProvider {
 	 *             not be found in the requested frame, an exception will be
 	 *             thrown.
 	 */
-	public TimedVector getRobot1Position(int frameNumber) throws Exception;
+	public TimedVector getRobot1Position(long frameNumber) throws Exception;
 
 	/**
 	 * Get the position of the 3rd (/4) robot from the left in the plating
@@ -54,7 +54,7 @@ public interface PositionProvider {
 	 *             not be found in the requested frame, an exception will be
 	 *             thrown.
 	 */
-	public TimedVector getRobot2Position(int frameNumber) throws Exception;
+	public TimedVector getRobot2Position(long frameNumber) throws Exception;
 
 	/**
 	 * Get the position of the 4th (/4) robot from the left in the plating
@@ -70,13 +70,13 @@ public interface PositionProvider {
 	 *             not be found in the requested frame, an exception will be
 	 *             thrown.
 	 */
-	public TimedVector getRobot3Position(int frameNumber) throws Exception;
+	public TimedVector getRobot3Position(long frameNumber) throws Exception;
 
 	/**
 	 * Get the position of the ball.
 	 * 
 	 * @param frameNumber
-	 *            The frame from which to get the position. 0 is the latest
+	 *            The frame from which to get the position.
 	 *            frame, 1 the frame before that and so on.
 	 * @return Returns the position of the object in the desired frame and the
 	 *         time at which the frame was captured.
@@ -85,6 +85,13 @@ public interface PositionProvider {
 	 *             not be found in the requested frame, an exception will be
 	 *             thrown.
 	 */
-	public TimedVector getBallPosition(int frameNumber) throws Exception;
+	public TimedVector getBallPosition(long frameNumber) throws Exception;
+	
+	/**
+	 * Get the number of the latest frame that was recorded.
+	 * 
+	 * @return Returns the number of the latest frame.
+	 */
+	public long getCurrentFrameNumber();
 
 }
