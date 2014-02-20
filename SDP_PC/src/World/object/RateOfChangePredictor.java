f98162d9eb1677ab89b7ed1990995be4a1a39552
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-02-20 15:38:42 +0000

    [WIP] Added a class to calculate velocities etc and estimate current positions etc.

---
+++ SDP_PC/src/World/object/RateOfChangePredictor.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package world.object;
+
+import geometry.Vector;
+import world.support.TimedVector;
+
+/**
+ * @author apljungquist
+ *
+ */
+public class RateOfChangePredictor {
+
+	/**
+	 * Predict the current value of a vector based on two previous values.
+	 * 
+	 * @param ultimate The last (or later) timed vector to use in the prediction.
+	 * @param penultimate The second to last (or earlier) timed vector to use in the prediction.
+	 * @return Returns a timed vector with the current time and the predicted value for that time.
+	 */
+	public static TimedVector findCurrent(TimedVector ultimate, TimedVector penultimate) {
+		//Calculate the change in time since the last record.
+		long deltaTime = System.currentTimeMillis()-ultimate.getTime();
+		//Get the rate of change between to two last records.
+		TimedVector rateOfChange = findRateOfChange(ultimate, penultimate);
+		//Assuming the rate of change is constant, calculate the current value
+		TimedVector current = ultimate.add(rateOfChange.scalarMultiplication(deltaTime/1000.0), deltaTime);
+		return current;
+	}
+	
+	/**
+	 * Calculates the rate of change between two timed vectors. The vector in the
+	 * result represents the rate of change per second. The time in the
+	 * resulting timed vector represents the time at which this rate of change existed.
+	 * This time is taken as the time of the ultimate timed vector.
+	 * 
+	 * @param ultimate
+	 *            The later of the two timed vectors (e.g. the most recent position)
+	 * @param penultimate
+	 *            The earlier of the two timed vectors (e.g. some historical position)
+	 * @return Returns a timed vector representing the rate of change at some point in
+	 *         time.
+	 */
+	public static TimedVector findRateOfChange(TimedVector ultimate, TimedVector penultimate) {
+		//Calculate the change in the vector and in the time.
+		TimedVector delta = ultimate.subtract(penultimate);
+		//Normalize the vector to represent the change over one second.
+		Vector rateOfChange = delta.getVector().scalarMultiplication(1000.0 / delta.getTime());
+		//Return a frame with the above rate of change as the vector and the time of the first given frame as time.
+		return new TimedVector(rateOfChange, ultimate.getTime());
+
+	}
+}
---
+++ SDP_PC/src/testing/executable/TestRateOfChangePredictor.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package testing.executable;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import world.object.RateOfChangePredictor;
+import world.support.TimedVector;
+
+/**
+ * @author apljungquist
+ *
+ */
+public class TestRateOfChangePredictor {
+
+	@Test
+	public void testFindRateOfChange() {
+		long now = System.currentTimeMillis();
+		TimedVector[] vectors = new TimedVector[6];
+		vectors[0] = new TimedVector(0,0, now-5000);
+		vectors[1] = new TimedVector(1,1, now-4000);
+		vectors[2] = new TimedVector(3,3, now-3000);
+		vectors[3] = new TimedVector(5,5, now-2000);
+		vectors[4] = new TimedVector(5,5, now-1000);
+		vectors[5] = new TimedVector(6,6, now-500);
+		
+		TimedVector[] rateOfChanges = new TimedVector[5];
+		rateOfChanges[0] = new TimedVector(1,1, now-4000);
+		rateOfChanges[1] = new TimedVector(2,2, now-3000);
+		rateOfChanges[2] = new TimedVector(2,2, now-2000);
+		rateOfChanges[3] = new TimedVector(0,0, now-1000);
+		rateOfChanges[4] = new TimedVector(2,2, now-500);
+		
+		assertEquals(rateOfChanges[0], RateOfChangePredictor.findRateOfChange(vectors[1], vectors[0]));
+		assertEquals(rateOfChanges[1], RateOfChangePredictor.findRateOfChange(vectors[2], vectors[1]));
+		assertEquals(rateOfChanges[2], RateOfChangePredictor.findRateOfChange(vectors[3], vectors[2]));
+		assertEquals(rateOfChanges[3], RateOfChangePredictor.findRateOfChange(vectors[4], vectors[3]));
+		assertEquals(rateOfChanges[4], RateOfChangePredictor.findRateOfChange(vectors[5], vectors[4]));
+	}
+	
+	@Test
+	public void testFindCurrent() {
+		long now = System.currentTimeMillis();
+		TimedVector[] vectors = new TimedVector[6];
+		vectors[0] = new TimedVector(0,0, now-5000);
+		vectors[1] = new TimedVector(1,1, now-4000);
+		vectors[2] = new TimedVector(3,3, now-3000);
+		vectors[3] = new TimedVector(5,5, now-2000);
+		vectors[4] = new TimedVector(5,5, now-1000);
+		vectors[5] = new TimedVector(6,6, now-500);
+		
+		TimedVector[] currents = new TimedVector[5];
+		currents[0] = new TimedVector(5,5, now);
+		currents[1] = new TimedVector(9,9, now);
+		currents[2] = new TimedVector(9,9, now);
+		currents[3] = new TimedVector(5,5, now);
+		currents[4] = new TimedVector(7,7, now);
+		
+		assertTrue(currents[0].approxEquals(RateOfChangePredictor.findCurrent(vectors[1], vectors[0])));
+		assertTrue(currents[1].approxEquals(RateOfChangePredictor.findCurrent(vectors[2], vectors[1])));
+		assertTrue(currents[2].approxEquals(RateOfChangePredictor.findCurrent(vectors[3], vectors[2])));
+		assertTrue(currents[3].approxEquals(RateOfChangePredictor.findCurrent(vectors[4], vectors[3])));
+		assertTrue(currents[4].approxEquals(RateOfChangePredictor.findCurrent(vectors[5], vectors[4])));
+	}
+
+}
