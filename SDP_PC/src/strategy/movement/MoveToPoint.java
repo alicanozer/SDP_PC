61f2813fdfdaf817f4f83e4ea59377789812fb87
EilidhHendry <ehendry91@gmail.com>
2014-02-25 20:13:02 +0000

    Fixing MoveToPoint to work with new bluetooth connections

--- SDP_PC/src/strategy/movement/MoveToPoint.java
+++ SDP_PC/src/strategy/movement/MoveToPoint.java
@@ -1,18 +1,19 @@
 package strategy.movement;
 
+import World.RobotType;
 import georegression.struct.point.Point2D_I32;
 import comms.BluetoothRobot;
 
 public class MoveToPoint {
-	
+
 	private static final int distanceFromPointToStop = 20;
 
-	public static void moveToPoint(BluetoothRobot bRobot, Point2D_I32 marker, Point2D_I32 dot, double pointX, double pointY) throws InterruptedException{
-		
+	public static void moveToPoint(BluetoothRobot robot, Point2D_I32 marker, Point2D_I32 dot, double pointX, double pointY) throws InterruptedException{
+
 		double distance = DistanceToBall.Distance(marker.x, marker.y, pointX, pointY);
 		System.out.println(String.format("Distance to point is %f", distance));
 		Point2D_I32 pointXY = new Point2D_I32((int)pointX,(int)pointY);
-		
+
 		double angle = TurnToObject.getAngleToObject(dot, marker, pointXY);
 		System.out.println(String
 				.format("Angle of point to robot is %f", angle));
@@ -20,24 +21,25 @@ public class MoveToPoint {
 		if (Math.abs(angle) > 20) {
 			// Turn to angle required
 			System.out.println("Stop and turn");
-			bRobot.stop();
-			bRobot.rotateLEFT((int)angle);
+			robot.stop("attack");
+			robot.rotateLEFT("attack", (int)angle);
 		}
 
 		while (distance > distanceFromPointToStop) {
 
 			angle = TurnToObject.getAngleToObject(dot, marker, pointXY);
 
-			if ((Math.abs(angle) > 15) && (Math.abs(angle) < 50)) {
 				// Stop everything and turn
 				System.out.println("The final angle is " + angle);
-				bRobot.stop();
-				bRobot.rotateLEFT((int) (angle / 2));
-			} else if (Math.abs(angle) > 50) {
-				bRobot.stop();
-				bRobot.rotateLEFT((int) angle);
-			}
-		
+				if (robot.type==RobotType.AttackUs) {
+					robot.stop("attack"); 
+					robot.rotateLEFT("attack", (int) (angle / 2));
+				}
+				else {
+					robot.stop("defence");
+					robot.rotateLEFT("defence", (int) (angle/2));
+				}
+
 			distance = DistanceToBall.Distance(marker.x, marker.y, pointX, pointY);
 			System.out.println("Distance to ball: " + distance);
 			Thread.sleep(100);
@@ -48,15 +50,19 @@ public class MoveToPoint {
 		if (Math.abs(angle) > 15) {
 			// Stop everything and turn
 			System.out.println("Making final correction");
-			bRobot.stop();
-			bRobot.rotateLEFT((int) angle);
-		} else {
-			bRobot.stop();
+			if (robot.type==RobotType.AttackUs) {
+				robot.stop("attack"); 
+				robot.rotateLEFT("attack", (int) (angle / 2));
+			}
+			else {
+				robot.stop("defence");
+				robot.rotateLEFT("defence", (int) (angle/2));
+			}
 		}
 		if (distance > distanceFromPointToStop) {
-			moveToPoint(bRobot, marker, dot, pointX, pointY);
+			moveToPoint(robot, marker, dot, pointX, pointY);
 		}
-		
+
 	}
-	
+
 }
