7faddef4144c1fc0ff776d824c2678964550c631
EilidhHendry <ehendry91@gmail.com>
2014-03-04 17:06:18 +0000

    Fixing strategies

---
+++ SDP_PC/src/strategy/planning/DefendPassD.java
@@ -0,0 +1,18 @@
+package strategy.planning;
+
+import movement.RobotMover;
+
+public class DefendPassD extends StrategyInterface{
+
+	public DefendPassD(RobotMover attackMover, RobotMover defenceMover) {
+		super(attackMover, defenceMover);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Starting DefendPassD (Defending against a pass with the defender)...");
+	}
+
+}
