034301764745a09342be2b0876feefd6a7060e2d
Anders-Petter Ljungquist <anderspetter.ljungquist@gmail.com>
2014-03-13 20:37:26 +0000

    Reversed some "side-effects" of the merge and removed some isolated, unused files that were added in the merge.

--- SDP_PC/src/comms/BluetoothRobot.java
+++ SDP_PC/src/comms/BluetoothRobot.java
@@ -3,15 +3,18 @@ package comms;
 import java.io.IOException;
 
 import lejos.pc.comm.NXTConnector;
+import World.Robot;
+import World.RobotType;
 
-public class BluetoothRobot implements RobotController {
+public class BluetoothRobot extends Robot implements RobotController {
 
 	private Bluetooth bluetooth;
 	public static final String TEAM_TRINITY = "0016530970C6";
 	public static final String HERCULES = "0016530D4ED8";
 	NXTConnector connector = new NXTConnector();
 	
-	public BluetoothRobot(Bluetooth bluetooth) {
+	public BluetoothRobot(RobotType type, Bluetooth bluetooth) {
+		super(type);
 		this.bluetooth = bluetooth;
 	}
 	
--- SDP_PC/src/sample/MixedStationaryObject.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import world.object.StationaryPixelObject;
-import world.object.StationaryRealObject;
-
-public interface MixedStationaryObject extends StationaryPixelObject,
-		StationaryRealObject {
-
-}
