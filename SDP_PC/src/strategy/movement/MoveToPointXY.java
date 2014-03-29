652656b1971a46c37b2c390ddc5e48fbe857127c
imanm93 <iman.m93@gmail.com>
2014-03-29 23:18:09 +0000

    Changes to Move to point, and tested grabbing

--- SDP_PC/src/strategy/movement/MoveToPointXY.java
+++ SDP_PC/src/strategy/movement/MoveToPointXY.java
@@ -11,82 +11,42 @@ public class MoveToPointXY {
 
 	private static final int distanceFromPointToStop = 5;
 	
-	public static void moveToPointXY(String type, RobotMover robotMover, Point2D_I32 dot, Point2D_I32 marker, Point2D_I32 point) throws Exception {
-		
-		double distance = DistanceCalculator.Distance(marker, point) - distanceFromPointToStop;
+//	public static void moveToPointXY(String type, RobotMover robotMover, Point2D_I32 dot, Point2D_I32 marker, Point2D_I32 point) throws Exception {
+//		
+//		double distance = DistanceCalculator.Distance(marker, point) - distanceFromPointToStop;
+//		
+//		double angle;
+//		
+//		// Initially turn to ball
+//		if (type.equals("attack")) {
+//			angle = TurnToObject.Point(RobotType.AttackUs);
+//		} else {
+//			angle =  TurnToObject.Ball(RobotType.DefendUs);
+//		} 
+//
+//		robotMover.rotate(type, (int) angle+5);
+//		System.out.println("Angle to Point:" + angle);										
+//						
+//	}
+	
+	public static void moveOurAttackToBall(String type, RobotMover robotMover) throws Exception {
 		
 		double angle;
 		
-		// Initially turn to ball
+		// Turn to ball
 		if (type.equals("attack")) {
 			angle = TurnToObject.Ball(RobotType.AttackUs);
 		} else {
 			angle =  TurnToObject.Ball(RobotType.DefendUs);
 		}
-		robotMover.rotate(type, (int) angle+5);
-		robotMover.waitForCompletion();
-		System.out.println("Angle to Point:" + angle);										
-		System.out.println("Distance to Point:" + distance);
 		
-		robotMover.setSpeedCoef(12);
-		robotMover.forward(type,distance); // we tell it to start moving forward
-		robotMover.waitForCompletion();
-		
-		// Move towards the ball till the distance is only the length of the robot to the ball
-//		while (distance > distanceFromPointToStop) {
-//			
-//			double angle;
-//			
-//			//Turn to Ball
-//			if (type.equals("attack")) {
-//				angle = TurnToObject.Ball(RobotType.AttackUs);
-//			} else {
-//				angle =  TurnToObject.Ball(RobotType.DefendUs);
-//			}
-//			
-//			robotMover.rotate(type, (int) angle+5);
-//			System.out.println("Angle to Point:" + angle);										
-//			System.out.println("Distance to Point:" + distance);
-//			robotMover.setSpeedCoef(20);
-//			robotMover.forward(type, distance);
-////			defenseMover.forward("defence", distance);
-//			robotMover.run();
-//			robotMover.waitForCompletion();
-//			System.out.println("All movements completed");
-////			defenseMover.waitForCompletion();
-//			
-//		}
-						
-	}
-public static void moveOurAttackToBall(String type, RobotMover robotMover) throws Exception {
-		
+		robotMover.rotate(type, (int) angle+3);
+		System.out.println("Angle to Ball:" + angle);										
+
+		//Move to Ball
 		double distance = DistanceCalculator.Distance(ObjectLocations.getUSAttack(), ObjectLocations.getBall());
-		
-		double angle;
-		
-		// Initially turn to ball
-		if (type.equals("attack")) {
-			angle = TurnToObject.Ball(RobotType.AttackUs);
-		} else {
-			angle =  TurnToObject.Ball(RobotType.DefendUs);
-		}
-		robotMover.rotate(type, (int) angle+5);
-		robotMover.waitForCompletion();
-		System.out.println("Angle to Point:" + angle);										
-		System.out.println("Distance to Point:" + distance);
-		robotMover.forward(type, distance/2);
-		if (type.equals("attack")) {
-			angle = TurnToObject.Ball(RobotType.AttackUs);
-		} else {
-			angle =  TurnToObject.Ball(RobotType.DefendUs);
-		}
-		System.out.println("Angle to Point:" + angle);										
-		if (angle > 15) {
-			robotMover.rotate(type, (int) angle);			
-		}
-		distance = DistanceCalculator.Distance(ObjectLocations.getUSAttack(), ObjectLocations.getBall());
-		System.out.println("Distance to Point:" + distance);
-		robotMover.forward(type, distance - 15);
+		System.out.println("Distance to Ball:" + distance);
+		robotMover.forward(type, distance - 19);
 		
 	}
 	
